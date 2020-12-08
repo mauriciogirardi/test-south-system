20ecf7257a989dfefbf3aa25e4f1ec9104c7abc2
Mauricio Girardi <maurigirarde@yahoo.com.br>
2020-12-08 20:35:46 -0300

    created global style

---
+++ src/styles/global.ts
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+*{
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #312e38  ;
+    color: #F4EDE8;
+    height: 100vh;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  h1,h2,h3,h4,h5,h6,strong {
+    font-weight: 500;
+  }
+
+  body,input,button, select {
+    font-family: 'Roboto', serif;
+    font-size: 16px;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #F4EDE8;
+  }
+`;
